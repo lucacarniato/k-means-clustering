451ffc1d834597af2ca96117c594363536990c84
lucacarniato <carniato.luca@gmail.com>
2021-08-10 10:01:04 +0200

    Improve documentation and code

--- KMeansMultiProcess.py
+++ KMeansMultiProcess.py
@@ -3,7 +3,6 @@ from ctypes import (c_int, c_double, CDLL, POINTER, cast)
 from multiprocessing import Process
 from multiprocessing.sharedctypes import RawArray
 
-
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +11,6 @@ import os
 
 def assign_points_to_cluster(num_dimension, num_clusters, start_index, end_index, centroids_coordinates, points,
                              squared_distances, min_distance_index):
-
     for i in range(start_index, end_index):
         point_index = i * num_dimension
         min_distance_index[i] = -1
@@ -29,55 +27,53 @@ def assign_points_to_cluster(num_dimension, num_clusters, start_index, end_index
                 min_distance_index[i] = j
                 squared_distances[i] = dis
 
+
 class KMeansMultiProcess:
 
-    def Set(self, num_centroids, input_points, num_iterations, num_processes, external_kernel=False, random_state =None):
+    def __init__(self, num_centroids, input_points, num_iterations, num_processes, external_kernel=False,
+                 random_state=None):
 
-        # set scalars
+        if input_points.shape[0] < num_centroids:
+            raise ValueError(
+                "Number of clusters k={} is smaller than number of data points n={}".format(self.num_clusters,
+                                                                                            self.num_points))
+        if input_points.shape[1] < 2:
+            raise ValueError("data points must have at least two dimensions")
+
+        self.num_iterations = num_iterations
         self.num_clusters = num_centroids
         self.num_dimension = input_points.shape[1]
         self.num_points = input_points.shape[0]
-        self.num_iterations = num_iterations
         self.num_processes = num_processes
         self.random_state = random_state
 
-        # set flat arrays (to avoid deep copies when interfacing with c++)
+        self.centroids = np.full(self.num_clusters * self.num_dimension, np.inf, dtype=np.double)
+        self.point_class = np.full(self.num_points, -1, dtype=np.int)
+        self.cluster_energy = np.zeros(self.num_clusters)
+        self.point_distances = np.zeros(self.num_points)
+        self.clusters_size = np.zeros(self.num_clusters, dtype=np.int)
+        self.clusters_sum = np.full(self.num_clusters * self.num_dimension, 0.0, dtype=np.double)
+
         if external_kernel:
-           self.points = np.full(self.num_points *self.num_dimension, np.inf, dtype=np.double)
+            self.points = np.full(self.num_points * self.num_dimension, np.inf, dtype=np.double)
+            self.squared_distances = np.full(self.num_points, math.inf, dtype=np.double)
+            self.min_distance_index = np.full(self.num_points, -1, dtype=np.int)
         else:
-           self.points = RawArray('d', np.full(self.num_points *self.num_dimension, np.inf, dtype=np.double))
+            self.points = RawArray('d', np.full(self.num_points * self.num_dimension, np.inf, dtype=np.double))
+            self.squared_distances = RawArray('d', np.full(self.num_points, math.inf, dtype=np.double))
+            self.min_distance_index = RawArray('i', np.full(self.num_points, -1, dtype=np.int))
 
         for i, point in enumerate(input_points):
             point_index = i * self.num_dimension
             for d in range(self.num_dimension):
                 self.points[point_index + d] = point[d]
 
-        self.centroids = np.full(self.num_clusters *self.num_dimension, np.inf, dtype=np.double)
-        self.point_class = np.full(self.num_points, -1, dtype=np.int)
-        self.cluster_energy = np.zeros(self.num_clusters)
-        self.point_distances = np.zeros(self.num_points )
-        self.clusters_size = np.zeros(self.num_clusters, dtype=np.int)
-        self.clusters_sum = np.full(self.num_clusters *self.num_dimension, 0.0, dtype=np.double)
-
-        if external_kernel:
-            self.squared_distances = np.full(self.num_points, math.inf, dtype=np.double)
-            self.min_distance_index =np.full(self.num_points, -1, dtype=np.int)
-        else:
-            self.squared_distances = RawArray('d',  np.full(self.num_points, math.inf, dtype=np.double))
-            self.min_distance_index = RawArray('i', np.full(self.num_points, -1, dtype=np.int))
-
-
-        if self.num_points < self.num_clusters:
-            raise ValueError("Number of clusters k={} is smaller than number of data points n={}".format(self.num_clusters, self.num_points))
-        if self.num_dimension < 2:
-            raise ValueError("data points must have at least two dimensions")
-
         self.initialize_centroids_k_means_pp()
         self.external_kernel = external_kernel
 
         if external_kernel:
             current_working_dir = os.getcwd()
-            dll_name='DistanceCalculator.dll'
+            dll_name = 'DistanceCalculator.dll'
             dll_path = os.path.join(current_working_dir, dll_name)
             self.kernelDll = CDLL(dll_path)
             print(self.kernelDll)
@@ -89,7 +85,7 @@ class KMeansMultiProcess:
             random.seed(self.random_state)
         first_index = random.randint(0, self.num_points)
 
-        #first centroid
+        # first centroid
         point_index = first_index * self.num_dimension
         for d in range(self.num_dimension):
             self.centroids[d] = self.points[point_index + d]
@@ -126,7 +122,7 @@ class KMeansMultiProcess:
     def spawn_process(self):
 
         self.processes = []
-        num_points_per_thread = math.ceil(self.num_points/self.num_processes)
+        num_points_per_thread = math.ceil(self.num_points / self.num_processes)
         start_index = 0
         for num_process in range(self.num_processes):
             end_index = start_index + num_points_per_thread
@@ -142,8 +138,7 @@ class KMeansMultiProcess:
                                                                                  self.min_distance_index)))
             start_index = end_index + 1
 
-        assert(end_index == self.num_points)
-
+        assert (end_index == self.num_points)
 
     def start_process(self):
         for p in self.processes:
@@ -162,7 +157,7 @@ class KMeansMultiProcess:
             min_distance_pointer = cast(self.squared_distances.ctypes.data, POINTER(c_double))
             min_distance_index_pointer = cast(self.min_distance_index.ctypes.data, POINTER(c_int))
 
-            #invoke the external distance calculator function
+            # invoke the external distance calculator function
             self.kernelDll.distance_calculator(c_int(self.num_clusters),
                                                c_int(self.num_points),
                                                c_int(self.num_dimension),
@@ -194,7 +189,7 @@ class KMeansMultiProcess:
                         for d in range(self.num_dimension):
                             self.clusters_sum[cluster_index + d] -= self.points[point_index + d]
 
-                    #assign new cluster and distances
+                    # assign new cluster and distances
                     self.point_class[i] = self.min_distance_index[i]
                     self.clusters_size[self.min_distance_index[i]] += 1
 
@@ -203,7 +198,7 @@ class KMeansMultiProcess:
                     for d in range(self.num_dimension):
                         self.clusters_sum[cluster_index + d] += self.points[point_index + d]
 
-                    self.point_distances[i] =self.squared_distances[i]
+                    self.point_distances[i] = self.squared_distances[i]
                     self.cluster_energy[self.min_distance_index[i]] += self.point_distances[i]
 
             # update centroids
@@ -217,31 +212,36 @@ class KMeansMultiProcess:
 
             # break iteration
             if diff < 1e-12:
-                print("Centroids unchanged at iteration ", iteration ," terminating...")
+                print("Centroids unchanged at iteration ", iteration, " terminating...")
                 break
+
 
 def plot_strong_scaling(n_samples, num_clusters, max_num_processes):
     global kMeansMultiProcess
     from sklearn.datasets.samples_generator import make_blobs
-    input_points, y_values = make_blobs(n_samples=n_samples, centers=num_clusters,cluster_std=0.4, random_state=42)
+    input_points, y_values = make_blobs(n_samples=n_samples, centers=num_clusters, cluster_std=0.4, random_state=42)
 
     times_multi_processes_python = np.empty((max_num_processes,))
     for num_processes in range(1, max_num_processes + 1):
         print("num_processes ", num_processes)
-        kMeansMultiProcess = KMeansMultiProcess()
-        kMeansMultiProcess.Set(num_clusters, input_points, num_iterations = 100, num_processes = num_processes, random_state = 42, external_kernel=False)
+        kMeansMultiProcess = KMeansMultiProcess(num_clusters, input_points, num_iterations=100,
+                                                num_processes=num_processes,
+                                                random_state=42, external_kernel=False)
         from timeit import timeit
-        times_multi_processes_python[num_processes-1] = timeit("kMeansMultiProcess.fit()", number=1,globals=globals())
+        times_multi_processes_python[num_processes - 1] = timeit("kMeansMultiProcess.fit()", number=1,
+                                                                 globals=globals())
     print(times_multi_processes_python)
 
-    times_multi_threaded_external= np.empty((max_num_processes,))
+    times_multi_threaded_external = np.empty((max_num_processes,))
     for num_processes in range(1, max_num_processes + 1):
         print(num_processes)
-        kMeansMultiProcess = KMeansMultiProcess()
-        kMeansMultiProcess.Set(num_clusters, input_points, num_iterations = 100, num_processes = num_processes, random_state = 42, external_kernel=True)
+        kMeansMultiProcess = KMeansMultiProcess(num_clusters, input_points, num_iterations=100,
+                                                num_processes=num_processes,
+                                                random_state=42, external_kernel=True)
         from timeit import timeit
-        times_multi_threaded_external[num_processes-1] = timeit("kMeansMultiProcess.fit()", number=1,globals=globals())
-    print(times_multi_threaded_external)
+        times_multi_threaded_external[num_processes - 1] = timeit("kMeansMultiProcess.fit()", number=1,
+                                                                  globals=globals())
+    #print(times_multi_threaded_external)
 
     plt.figure(figsize=(10, 4))
     ax = plt.axes()
@@ -257,22 +257,25 @@ def plot_strong_scaling(n_samples, num_clusters, max_num_processes):
     plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
     plt.legend(loc=2)
 
-def plot_results(n_samples, num_clusters, num_processes, external_kernel):
 
+def plot_results(n_samples, num_clusters, num_processes, external_kernel):
     from sklearn.datasets.samples_generator import make_blobs
-    input_points, labels, true_centroids = make_blobs(n_samples=n_samples, centers=num_clusters,cluster_std=0.4, random_state=0, return_centers = True)
+    input_points, labels, true_centroids = make_blobs(n_samples=n_samples, centers=num_clusters, cluster_std=0.4,
+                                                      random_state=0, return_centers=True)
     kMeansMultiProcess = KMeansMultiProcess()
-    kMeansMultiProcess.Set(num_clusters, input_points, num_iterations=100, num_processes=num_processes, external_kernel=external_kernel)
+    kMeansMultiProcess.Set(num_clusters, input_points, num_iterations=100, num_processes=num_processes,
+                           external_kernel=external_kernel)
     kMeansMultiProcess.fit()
 
     print("Plotting...")
-    cross_color ='k'
-    calculated_centroids = np.reshape(kMeansMultiProcess.centroids,(kMeansMultiProcess.num_clusters,kMeansMultiProcess.num_dimension))
+    cross_color = 'k'
+    calculated_centroids = np.reshape(kMeansMultiProcess.centroids,
+                                      (kMeansMultiProcess.num_clusters, kMeansMultiProcess.num_dimension))
 
     plt.scatter(calculated_centroids[:, 0], calculated_centroids[:, 1],
-                marker='x', s=50, linewidths=50,color=cross_color, zorder=11, alpha=1)
+                marker='x', s=50, linewidths=50, color=cross_color, zorder=11, alpha=1)
 
     plt.scatter(true_centroids[:, 0], true_centroids[:, 1],
-                marker='+', s=50, linewidths=50,color='r', zorder=11, alpha=1)
+                marker='+', s=50, linewidths=50, color='r', zorder=11, alpha=1)
 
     plt.plot(input_points[:, 0], input_points[:, 1], 'k.', markersize=2)
--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Multi process K-Means clustering
 
-This project implements the KMeans clustering algorithm using multiple processes. The following steps are followed:
+This project implements the KMeans clustering algorithm using multiple processes. The following steps are the following:
 
 * Initialize the centroids for the predefined number of clusters.
-* Calculate the distances of each point from the centroids and assign each point to the closest centroid. All points assigned to the same centroid represent a cluster. This part of the algorithm is the most computing intensive and was parallelized.
+* Calculate the distances of each point from the centroids and assign each point to the closest centroid. 
+All points assigned to the same centroid represent a cluster. This part of the algorithm is the most computing intensive and was parallelized.
 * Updated the coordinates of the centroids with the mean x and y values of all points in the cluster. 
 * Iterate from step 2 to 4 until the position of the centroids does not change or the maximum number of iterations is reached.
 
-Centroid initialization follows the k-means++ algorithm (http://ilpubs.stanford.edu:8090/778/), where the centroids positions are initialized far from each other. 
+Centroid initialization follows the k-means++ algorithm (http://ilpubs.stanford.edu:8090/778/), 
+where the centroids positions are initialized as far as possible. 
 
-As can be seen from the figure below, the multiprocess implementation does not scale well, because large arrays between Python processes needs to be serialized into strings of bytes using pickle. 
+The multiprocess implementation does not scale well, because large arrays between Python processes needs to be serialized into strings of bytes using pickle. 
 
 ![alt text](https://github.com/lucacarniato/Multithreaded_K-Means_clustering/blob/master/WallClockTime.png)
 
